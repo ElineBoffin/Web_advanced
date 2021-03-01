d8349acffc7324213b2d647a540f5b74ad371cb1
Eline-B <eline.boffin@telenet.be>
2021-03-01 12:17:08 +0100

    Oefeningen js 3

--- JS_deel_3/oefeningen_functions2.js
+++ JS_deel_3/oefeningen_functions2.js
@@ -4,45 +4,75 @@
 // Schrijf een functie 'multiply()'
 // 2 parameters (number,number)
 // output: De vermenigvuldiging van de twee getallen die als input gegeven werden.
-
+function multiply(number1, number2) {
+    return number1 * number2;
+}
 
 // Schrijf een functie 'difference()'
 // 1 parameter
 // output: Het verschil tussen het ingegeven getal en het getal 100.
+function difference(verschil) {
 
+}
 
 // Schrijf een functie 'isFifty()'
 // 2 parameters
 // output: Geeft TRUE terug als één van de getallen '50' is OF de som van de 2 getallen '50' is.
+function isFifty() {
 
+}
 
 // Schrijf een functie 'startsWithHello()'
 // 1 parameter
 // output: Als de inputstring begint met 'Hello ', return gewoon de string. Begint ze niet met 'Hello ', plak dit er dan voor.
+function startsWithHello() {
 
+}
 
 // Schrijf een functie 'removeCharacter()'
 // 2 parameters (string, number)
 // output: Verwijder het zoveelste karakter uit je string en print de nieuwe string (met dus 1 karakter minder)
 // TIP: Kijk naar String.substring of String.slice
+function removeCharacter(string, number) {
 
+}
 
 // Schrijf een functie 'capitalizeEveryWord()'.
 // 1 parameter (string)
 // output: Elk woord in de inputstring start met een hoofdletter, de rest van de letters zijn een kleine letter.
 // TIP: Zoek String.prototype.split() , String.charAt() , String.substring() en String.join() op
+function capitalizeEveryWord(string) {
 
+}
 
 // Schrijf een functie 'countVowels()'.
 // 1 parameter (string)
 // output: Het aantal klinkers dat voorkomt in de input string.
 // TIP: Loop over je string en kijk voor elke letter of die overeen komt met één van de klinkers.
+function countVowels(str) {
+    let vowelsCount = 0;
 
+    //turn the input into a string
+    let string = str.toString();
+
+    //loop through the string
+    for (let i = 0; i <= string.length - 1; i++) {
+
+        //if a vowel, add to vowel count
+        if (string.charAt(i) === "a" || string.charAt(i) === "e" || string.charAt(i) === "i" || string.charAt(i) === "o" || string.charAt(i) === "u") {
+            vowelsCount += 1;
+        }
+    }
+    return vowelsCount;
+}
 
 // Schrijf een functie 'maxValue()'
 // 3 parameters
 // output: De functie geeft het grootste getal van de 3 als resultaat
-
+function maxValue(number1, number2, number3) {
+    let result = Math.max(number1, number2, number3);
+    return result;
+}
 
 
 console.log('Output oef 1:', multiply(2, 2), multiply(4, 12));
@@ -50,7 +80,7 @@ console.log('Output oef 2:', difference(156), difference(20));
 console.log('Output oef 3:', isFifty(15, 35), isFifty(70, -20), isFifty(50, 180), isFifty(10, 12));
 console.log('Output oef 4:', startsWithHello('Hello there'), startsWithHello('there'));
 console.log('Output oef 5(a):', removeCharacter('Voorbeeld', 1), removeCharacter('Voorbeeld', 4));
-console.log('Output oef 5(b):', removeCharacter2('Voorbeeld', 1), removeCharacter2('Voorbeeld', 4));
+//console.log('Output oef 5(b):', removeCharacter2('Voorbeeld', 1), removeCharacter2('Voorbeeld', 4));
 console.log('Output oef 6:', capitalizeEveryWord('elk woord zou moeten beginnen met een hoofdletter'));
 console.log('Output oef 7:', `De inputstring bevat ${countVowels('oefening')} klinkers`);
 console.log('Output oef 8:', `Het grootste nummer is ${maxValue(12, 5, 25)}`);
